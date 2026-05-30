934d9f8ef1c0cebf56e331fc362a4d16c234eef1
Adriany de Queiroz Silva <adriany.silvaqs@gmail.com>
2026-05-30 11:23:21 -0300

    javascript e estilo

---
+++ tela inicial/script.js
@@ -0,0 +1,27 @@
+const btnAcess = document.getElementById("btnAcess");
+const menuAcess = document.getElementById("menuAcess")
+let tamanhoFonte = 16;
+
+btnAcess.addEventListener("click", function (){
+    menuAcess.classList.toggle("hidden");
+});
+// modo escuro
+document.getElementById("modoEscuro").addEventListener("click", function(){
+    document.body.classList.toggle("dark");
+})
+// aumentar fonte
+document.getElementById("aumentarFonte").addEventListener("click", function(){
+    if( tamanhoFonte <=20 ){
+        tamanhoFonte +=2;
+    document.body.style.fontSize = tamanhoFonte + "px";
+    }
+    
+});
+// diminuirr fonte
+document.getElementById("diminuirFonte").addEventListener("click", function(){
+    if (tamanhoFonte > 12){
+        tamanhoFonte -=2;
+    document.body.style.fontSize = tamanhoFonte + "px";
+    }
+    
+});
